b3d8997428cb38ddb93d10976772fb9d86d1cc78
Your Name <your.email@example.com>
2026-07-17 18:09:52 +0600

    Initial commit

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'daily-tracker-v1';
+const APP_SHELL = [
+  './',
+  './daily-tracker.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request)).then(response => {
+      if (response) return response;
+      return caches.match('./daily-tracker.html');
+    })
+  );
+});
